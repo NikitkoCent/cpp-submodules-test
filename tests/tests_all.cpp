4d840ca6cd3e3e29bbd3150aaa41489c9988519f
Nikita Provotorov <nikita.provotorov.work@gmail.com>
2021-11-26 15:49:29 +0700

    Add more tests.

--- tests/tests_all.cpp
+++ tests/tests_all.cpp
@@ -4,3 +4,11 @@
 TEST(MYFUNC_SUM, ZERO_PLUS_ZERO_IS_ZERO) {
     ASSERT_EQ(myfunc::sum(0, 0), 0);
 }
+
+TEST(MYFUNC_SUM, SUM_10_M10) {
+    ASSERT_EQ(myfunc::sum(10, -10), 0);
+}
+
+TEST(MYFUNC_SUM, SUM_15_M10) {
+    ASSERT_NE(myfunc::sum(15, -10), 0);
+}
